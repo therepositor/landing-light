8d325dc70296b0493c4b4acc32db694282387939
Joseph <joseph.oguntebi@yahoo.com>
2022-02-03 23:22:38 +0100

    styleLargeScreens

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
 import Index from './routes/Index'
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [cardDetails, setCardDetails] = useState({});
+  const [state, setState] = useState({
+    securityCode: '',
+    expirationDate: '',
+    creditCardNumber: '',
+    postalCode: '',
+  });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    let value = e.target.value;
+    console.log(value);
+    if(e.target.name === 'securityCode'){
+      setState({
+        ...state,
+        [e.target.name]: value
+      })
+    } else if (e.target.name === 'expirationDate') {
+      if(value.length === 2 ) {
+        value += '/';
+      }
+      setState({
+        ...state,
+        [e.target.name]: value
+      })
+    } else if (e.target.name === 'creditCardNumber') {
+      if (value.length % 4 === 0) {
+        value += ' ';
+      }
+      setState({
+        ...state,
+        [e.target.name]: value
+      })
+    } else {
+      setState({
+        ...state,
+        [e.target.name]: value
+      })
+    }
+  }
+  const handleSubmit = (e) => {
+    e.preventDefaut();
+    console.log(e, "handleSubmit!!!")
+    setCardDetails({
+      ...cardDetails, state
+    })
+  };
+  console.log(state)
+  console.log(cardDetails)
   return (
     <div className="App">
-      <Index />
+      <Index handleSubmit={handleSubmit} handleChange={handleChange} state={state}/>
     </div>
   );
 }
--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './../components/Header';
 import Main from '../components/Main';
 
-const Index = () => (
+const Index = ({handleChange, state, handleSubmit}) => (
     <BrowserRouter>
         <Header></Header>
         <Routes>
-            <Route path='/' element={<Main />} />
+            <Route  path='/' element={<Main  handleChange={handleChange} handleSubmit={handleSubmit} state={state} />} />
         </Routes>
     </BrowserRouter>
 );
